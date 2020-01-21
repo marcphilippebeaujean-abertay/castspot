68b1ce56f7faa9f975dc5859d9871efa184529c4
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-01-21 07:58:50 +0100

    added environment variable for prod mode

--- castspot_project/settings.py
+++ castspot_project/settings.py
@@ -7,9 +7,11 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True if os.environ.get('IS_PROD') == 'FALSE' else False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'castspot.onrender.com'
+]
 
 
 INSTALLED_APPS = [

8402f06f5fe5facbc89b7fe76fea77f45efa722b
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-03-07 13:52:21 +0100

    added caching for guest speaking board

--- castspot_project/settings.py
+++ castspot_project/settings.py
@@ -118,6 +118,13 @@ DATABASES = {
     }
 }
 
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django.core.cache.backends.memcached.MemcachedCache',
+#        'LOCATION': '127.0.0.1:11211',
+#    }
+#}
+
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = []

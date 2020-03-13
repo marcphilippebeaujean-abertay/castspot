43b382f29eb4c5008e140a616b2b05c6e0dc7ff1
marcphilippebeaujean-abertay <marcphilippebeaujean@gmail.com>
2020-03-13 21:49:11 +0100

    added script to install the node modules

--- util_scripts/build_frontend/build_frontend.sh
+++ util_scripts/build_frontend/build_frontend.sh
@@ -4,7 +4,7 @@ PROJECT_DIR_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../.." >/dev/null 2>&1
 FRONTEND_PROJECT_DIR_PATH="${PROJECT_DIR_PATH}/frontend"
 
 cd "${FRONTEND_PROJECT_DIR_PATH}" || exit 1
+yarn install
 yarn build
-# shellcheck disable=SC2164
 cd "${PROJECT_DIR_PATH}"
 python "${PROJECT_DIR_PATH}/util_scripts/build_frontend/move_files.py"
